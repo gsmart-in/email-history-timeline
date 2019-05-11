64120be32685b0e50b1b83fbde53fcd313412d09
Prasanth Janardanan <prasanthmj@gmail.com>
2019-05-11 16:09:46 +0530

    Trimmed the end section to just a simple quote.

--- src/partials/end.jsx
+++ src/partials/end.jsx
@@ -9,43 +9,8 @@ export default class End extends React.Component
 		<div class="columns" style={{marginTop:'3rem'}} >
 		<div class="column is-narrow"></div>
 		<div class="column is-6 content">
-		<h3>Present</h3>
-		<p>
-		Cloud-based services started catching up starting 2006. More and more companies moved on to cloud-based services rather than on-premise or self-hosted email. This trend is predicted to continue. 
-		</p>
-		<p>
-		As we go through the history of Email, certain points emerge. Email is not the creation of a single person or company or organization. There are a large number of people that played roles in its evolution. Talking of evolution, Email is an evolving solution that has faced a number of challenges and evolved and became stronger over time. The primary strength of email is that it is based on open standards If you count, there are at least 15 RFC standards related to email. The open standards helped developing software that works together, both in open source and in the proprietary world. Another strength is the universal support for email. There are email clients in almost all platforms. All programming platforms support email out of the box.
-		</p>
-		<p>
-		There was a consistent effort to make email better. By late 2000s email spam was becoming a huge problem. As much as 90% email traffic was spam
-		As it evolved more spam filters and methods  came into picture Spam filtering grew from Baysian filters to AI based training. 
-		</p>
-		<h3>The future of Email</h3>
-		<p>
-		There are at least two concerns that have not been solved as far as email is concerned.
-		</p>
-
-		<h5>End to end encryption </h5>
-		<p>The efforts to standardise  email encryption can be traced back to the beginning of email itself. However, owing to various reasons, end to end encrypted email has not become as universal as it should be. This is one area that needs more efforts. 
-		</p>
-
-		<h5>Certified Email</h5>
-		<p>
-		The Concept of Certified email has not developed as  much as it should be.
-		A certified email is perfect when you need a proof and a certification that your documents have been seen and delivered. It's like a certified or registered post mail, but, of course, a lot quicker and cheaper. 
-		</p>
-
-		<p>
-		Just as the certified post mail, this is valid in legal proceedings and can be used to justify that a document or article was delivered or that a delivery attempt was made. 
-		The related standard RFC 6109. Hong Kong and Italy are some of the early adopters of Certified email.
-		</p>
-		<h3>More Email Clients, More Email Services</h3>
-		<p>
-		You know when software is doomed - when a specific vendor takes it all and other smaller players slowly disappear. At the moment, Gmail has 1.2 billion users and is #2 email client on mobile devices and perhaps #1 web email.  We need more and more email services targeting specific requirement points (privacy, ease of use, automation features) and thrive in a multi-vendor environment. Better Email clients are possible and more people would want that if only somebody built such a thing. A better, private, secure email service is possible if only somebody came up with that.  
-		</p>
-		<p>
-		Let us hope the future of Email has better services for us.
-		</p>
+		<h3>&ldquo;Instead of worrying about what you cannot control, shift your energy to what you can create.<br/> <br/>
+		What you do today could be history tomorrow&rdquo;</h3>
 
 		</div>
 		</div>
